20fb2978d9b0ec4ed57c9b96cb2f4f34f59f7a2b
Nicolas Bonnici <nicolasbonnici@gmail.com>
2014-04-10 05:17:03 +0200

    "Auto generated commit message for sample bundle by sociableCore framework console app, see diff for more infos"

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -18,54 +18,6 @@ class HomeController extends \Library\Core\Auth
 
     public function indexAction()
     {
-
-        $sJsMinFile  = ROOT_PATH . 'public/lib/sociableUx/build/sociable.ux.min.js';
-        $sCssMinFile = ROOT_PATH . 'public/lib/sociableUx/build/sociable.ux.min.css';
-        $sMinifiedJsCode = '';
-        $sMinifiedCssCode = '';
-
-        $aSociableUxJsLibs = array(
-            '/lib/js/jquery-1.11.js',
-            '/lib/plugins/layout/js/jquery.layout.min.js',
-            '/lib/plugins/bootstrap3/js/bootstrap.js',
-            '/lib/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
-            '/lib/plugins/pnotify/js/jquery.pnotify.js',
-            '/lib/sociableUx/js/ux.core.js',
-            '/lib/sociableUx/js/charts.core.js',
-            '/lib/sociableUx/js/core.js'
-        );
-        $aSociableUxCssLibs = array(
-            '/lib/sociableUx/css/core.classes.css',
-            '/lib/sociableUx/css/core.ui.css'
-        );
-
-        foreach ($aSociableUxJsLibs as $sJsLibPath) {
-            $sMinifiedJsCode .= \Library\Core\Minify::js(file_get_contents(ROOT_PATH . 'public' . $sJsLibPath));
-        }
-        foreach ($aSociableUxCssLibs as $sCssLibPath) {
-            $sMinifiedCssCode .= \Library\Core\Minify::css(file_get_contents(ROOT_PATH . 'public' . $sCssLibPath));
-        }
-
-        if (!$oHandle = fopen($sJsMinFile, 'a+')) {
-            echo "Impossible d'ouvrir le fichier ($sJsMinFile)";
-            exit;
-        }
-        if (fwrite($oHandle, $sMinifiedJsCode) === FALSE) {
-            echo "Impossible d'écrire dans le fichier ($sJsMinFile)";
-            exit;
-        }
-
-        if (!$oHandle = fopen($sCssMinFile, 'a+')) {
-            echo "Impossible d'ouvrir le fichier ($sCssMinFile)";
-            exit;
-        }
-        if (fwrite($oHandle, $sMinifiedCssCode) === FALSE) {
-            echo "Impossible d'écrire dans le fichier ($sCssMinFile)";
-            exit;
-        }
-
-        fclose($oHandle);
-
         $this->render('sample/index.tpl');
     }
 }
