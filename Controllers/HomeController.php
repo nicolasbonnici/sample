79e5491f727d7d8b9c3f66303e1ef0d64b925865
Nicolas Bonnici <nicolasbonnici@gmail.com>
2014-09-08 21:23:04 +0200

    "Auto generated commit message for sample bundle by sociableCore framework console app, see diff for more infos"

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends \Library\Core\Auth
 
     public function indexAction()
     {
-        $this->oView->render($this->aView, 'sample/index.tpl');
+        $this->oView->render($this->aView, 'home/index.tpl');
     }
 }
 
